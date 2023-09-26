bf4d747ec4e210d03615076a40373abcaf0a3eb8
Delvina J <j.delvina@gmail.com>
2023-09-26 18:18:31 +0700

    add: badge

---
+++ Landmarks/Views/Helper/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Delvina Janice on 26/09/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
